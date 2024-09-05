ff5439c3af5fddc0de457d4486bc0757775cacec
Faruk Yazici <faruk.yazici@eteration.com>
2024-09-05 21:38:03 +0200

    Add home page container

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
-import { SafeAreaView, StatusBar } from 'react-native'
+import { StatusBar, SafeAreaView, StyleSheet } from 'react-native'
+import { HomePage } from './containers'
+import { colors } from './theme'
 
 const App: React.JSX.Element = () => (
-  <SafeAreaView>
+  <SafeAreaView style={styles.container}>
     <StatusBar />
+    <HomePage />
   </SafeAreaView>
 )
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.cyan2
+  }
+})
 
 export default App
