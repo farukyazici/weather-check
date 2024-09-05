a060a2f050db6383c6fd79c813a828f9f3e9eb83
Faruk Yazici <faruk.yazici@eteration.com>
2024-09-05 20:52:43 +0200

    Set up Eslint and remove boiler code

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { SafeAreaView, StatusBar } from 'react-native'
+
+const App: React.JSX.Element = () => (
+  <SafeAreaView>
+    <StatusBar />
+  </SafeAreaView>
+)
+
+export default App
